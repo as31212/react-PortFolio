50bb7166c6862f0600c28d5b719d7cf68bd63b71
Ahmad Searcy <ahmadsearcy7@gmail.com>
2024-05-02 15:09:53 -0500

    project component complete

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,10 @@
-const Project = ({ img, link, description, stack }) => {
+const Project = ({ img, link, description, stack, title }) => {
     return (
       <>
         <div className="project bg-gray-200 shadow-xl flex flex-row p-5 ease-in-out hover:scale-105 transition duration-300 gap-4 m-20 rounded-lg">
           <a target="_blank" href={link}><img src={img} className="w-1/3 m-0 p-0" alt="Project Img" /></a>
           <div className="gap-10 flex flex-col">
+            <h2><b>Title: </b>{title}</h2>
             <p><b>Tech Stack:</b> {stack}</p>
             <p><b>Description:</b> {description}</p>
           </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,6 +9,7 @@ const Projects = () => {
           link="https://shop-it-beta.netlify.app/"
           description="Experience the showcase of my front-end expertise with this Mock E-commerce. Seamlessly blending sleek design with intuitive functionality, this project demonstrates my ability to create immersive online experiences. Explore curated products, captivating visuals, and seamless navigation, reflecting my commitment to excellence in front-end development."
           stack="React, React-Router, Bootstrap CSS"
+          title="Shop-IT"
         />
       </div>
     </>
